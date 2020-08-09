d8349c225501487acde9491adbe298e9c118653d
Evan Stinger <evanstinger@Evans-MacBook-Air.local>
2020-08-09 21:13:46 +0700

    cleaning repo, ready to remote

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+// thousand separators
+function thousands_separators(num) {
+  var num_parts = num.toString().split(".");
+  num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num_parts.join(".");
+}
 
 !(function($) {
   "use strict";
@@ -51,8 +57,108 @@
     }
   });
 
-  // Activate smooth scroll on page load with hash links in the url
+  
   $(document).ready(function() {
+    
+
+    let mintedSupply = '';
+    let burnedSupply = '';
+    let transactions = '';
+    let holders = '';
+
+    //calling eIDR TRC10 information
+
+    $.ajax({
+
+      url: 'https://apilist.tronscan.org/api/token',
+      type: 'get',
+      dataType: 'json',
+      data: {
+        'id': '1002652'
+      },
+      success: function (result) {
+        setTimeout(function () {
+          //calling mintedSupply
+          mintedSupply += result.data[0].issued / 100;
+          $('#minted').html(thousands_separators(mintedSupply));
+
+          //calling transactions
+          transactions += result.data[0].totalTransactions;
+          $('#transactions').html(thousands_separators(transactions));
+          //calling Holders
+          holders += result.data[0].nrOfTokenHolders;
+          $('#holders').html(thousands_separators(holders));
+        }, 1000);
+
+      }
+    });
+
+    //calling burnedSupply
+
+    $.ajax({
+
+      url: 'https://apilist.tronscan.org/api/account',
+      type: 'get',
+      dataType: 'json',
+      data: {
+        'address': 'TLsV52sRDL79HXGGm9yzwKibb6BeruhUzy'
+      },
+      success: function (result) {
+        setTimeout(function () {
+          burnedSupply += result.balances[617].balance / 100;
+          $('#burned').html(thousands_separators(burnedSupply));
+        }, 1000);
+
+        setTimeout(function(){
+          $('#circulating').html(thousands_separators(mintedSupply - burnedSupply))
+        },1200)
+
+      }
+
+    });
+
+    //calling latestTransactions
+    $.ajax({
+
+      url: 'https://apilist.tronscan.org/api/asset/transfer',
+      type: 'get',
+      dataType: 'json',
+      data: {
+        'limit': '5',
+        'start': '0',
+        'name': 'eIDR',
+        'issueAddress': 'TPz5uVytAaUup9vTydcqscyj3kqSeekKvn'
+      },
+      success: function (result) {
+
+        let txs = result.Data;
+
+        $.each(txs, function (i, data) {
+          let waktu = formatTime(data.timestamp);
+
+          $('#transactionList').append(`
+            <a href="#" class="list-group-item list-group-item-action">
+              <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">`+ thousands_separators(data.amount / 100) + ` eIDR</h5>
+                <small class="timeago">`+ waktu + `</small>
+              </div>
+              <p class="mb-1">
+              Dari: `+ data.transferFromAddress + `<br>
+              Ke: `+ data.transferToAddress + ` <br>
+              </p>
+              <small class="text-break">HASH: `+ data.transactionHash + `</small>
+            </a>
+
+
+          `);
+
+        });
+      }
+
+    });
+    
+    // Activate smooth scroll on page load with hash links in the url
+    
     if (window.location.hash) {
       var initial_nav = window.location.hash;
       if ($(initial_nav).length) {
@@ -124,14 +230,7 @@
     time: 1000
   });
 
-  // Skills section
-  $('.skills-content').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  }, {
-    offset: '80%'
-  });
+  
 
   // Init AOS
   function aos_init() {
@@ -141,46 +240,208 @@
     });
   }
 
-  // Porfolio isotope and filter
+  // Onload
   $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $('.venobox').venobox({
-      'share': false
-    });
 
     // Initiate aos_init() function
     aos_init();
 
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
 
 })(jQuery);
+
+// copy button at donate address
+document.getElementById("copy-btn").addEventListener("click", copy_address);
+
+function copy_address() {
+  var copyText = document.getElementById("donateAddress");
+  var textArea = document.createElement("textarea");
+  textArea.value = copyText.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("Copy");
+  textArea.remove();
+
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to Snackbar DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from Snackbar DIV
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+  //AJAX search Address//
+  function searchAddress() {
+
+    $('.modal-body').html('');
+    $('#modal-title').text($('#search-input').val());
+    let eIDRbalance = 0;
+    let LMBbalance = 0;
+
+    $.ajax({
+      url: 'https://apilist.tronscan.org/api/account',
+      type: 'get',
+      dataType: 'json',
+      data: {
+        'address': $('#search-input').val()
+      },
+      success: function (result) {
+        // get date created
+        let dateCreated = new Date(result.date_created);
+
+        // get transaction counts
+        let transactionCounts = result.totalTransactionCount;
+
+        let tokenBalancesArray = result.tokenBalances;
+
+        //check if eIDR exist
+        let eIDRexist = tokenBalancesArray.some(function(tokenID){
+          return tokenID.name == '1002652';
+        })
+
+        if (eIDRexist) {
+          // get eIDR Balance
+          let eIDRarray = tokenBalancesArray.find(function (tokenID) {
+            return tokenID.name == '1002652';
+          })
+          eIDRbalance = eIDRarray.balance / 100 + ' eIDR';
+        }
+
+        //check if LMB exist
+        let LMBexist = tokenBalancesArray.some(function (tokenID) {
+          return tokenID.name == '1002640';
+        })
+
+        if (LMBexist) {
+          // get LMB Balance
+          let LMBarray = tokenBalancesArray.find(function (tokenID) {
+            return tokenID.name == '1002640';
+          })
+          LMBbalance = LMBarray.balance / 1000000 + ' LMB';
+        }
+
+        
+        
+        
+        //put all of those above into modal box
+        $('.modal-body').html(`
+        
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Saldo eIDR: <strong>`+ thousands_separators(eIDRbalance) +`</strong></li>
+            <li class="list-group-item">Saldo LMB: <strong>`+ thousands_separators(LMBbalance) +`</strong></li>
+            <li class="list-group-item">Total Transaksi: `+ transactionCounts +` kali</li>
+            <li class="list-group-item">Dibuat sejak: <small>`+ dateCreated +`</small></li>
+          </ul>
+        `)
+      },
+    });
+
+    $('#search-input').val('');
+
+  }
+
+//AJAX search HASH//
+function searchHash() {
+
+  $('.modal-body').html('');
+  $('#modal-title').text($('#search-input').val());
+  let nominal = '';
+  let token = '';
+
+  $.ajax({
+    url: 'https://apilist.tronscan.org/api/transaction-info',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      'hash': $('#search-input').val()
+    },
+    success: function (result) {
+      let tipeTransaksi = result.contractData;
+      let waktu = new Date(result.timestamp);
+      if (tipeTransaksi.asset_name == "1002652") {
+        nominal = tipeTransaksi.amount / 100;
+        token = ' eIDR';
+      } else if (tipeTransaksi.asset_name == "1002640") {
+        nominal = tipeTransaksi.amount / 1000000;
+        token = ' LMB';
+      }else {
+        nominal = tipeTransaksi.amount;
+        token = tipeTransaksi.asset_name;
+      }
+      
+      //put all of those above into modal box
+      $('.modal-body').html(`
+        
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Pengirim: <strong>`+ result.ownerAddress + `</strong></li>
+            <li class="list-group-item">Penerima: <strong>`+ result.toAddress + `</strong></li>
+            <li class="list-group-item">Nominal Transfer: <strong>`+ thousands_separators(nominal) + token + `</strong> </li>
+            <li class="list-group-item">Waktu Transaksi: `+ waktu + ` </li>
+            <li class="list-group-item">Status Konfirmasi: `+ result.confirmed + `</li>
+            <li class="list-group-item">Keterangan: `+ result.data + `</li>
+          </ul>
+        `)
+    },
+  });
+
+  $('#search-input').val('');
+
+}
+  
+//Search Button Function
+$('#search-button').on('click', function(){
+  //Check if input === Address
+  if ($('#search-input').val().length == 34) {
+    searchAddress();
+  } else if ($('#search-input').val().length == 64) {
+    searchHash();
+  } else {
+    $('#search-input').val('');
+    $('#modal-title').text('Ada yang salah, nih!')
+    $('.modal-body').html(`
+      <p>Periksa kembali alamat atau Hash yang anda masukkan.</p>
+    `);
+  }
+  
+});
+
+$('#search-input').change(function(){
+  
+  if ($('#search-input').val().includes('tronscan')) {
+    let hashOnly = $('#search-input').val().split('/')[5];
+    $('#search-input').val(hashOnly);
+  }
+})
+
+
+
+
+// timeago
+var periods = {
+  month: 30 * 24 * 60 * 60 * 1000,
+  week: 7 * 24 * 60 * 60 * 1000,
+  day: 24 * 60 * 60 * 1000,
+  hour: 60 * 60 * 1000,
+  minute: 60 * 1000
+};
+
+function formatTime(timeCreated) {
+  var diff = Date.now() - timeCreated;
+
+  if (diff > periods.month) {
+    // it was at least a month ago
+    return Math.floor(diff / periods.month) + " bulan yang lalu";
+  } else if (diff > periods.week) {
+    return Math.floor(diff / periods.week) + " minggu yang lalu";
+  } else if (diff > periods.day) {
+    return Math.floor(diff / periods.day) + " hari yang lalu";
+  } else if (diff > periods.hour) {
+    return Math.floor(diff / periods.hour) + " jam yang lalu";
+  } else if (diff > periods.minute) {
+    return Math.floor(diff / periods.minute) + " menit yang lalu";
+  }
+  return "Baru saja";
+}
